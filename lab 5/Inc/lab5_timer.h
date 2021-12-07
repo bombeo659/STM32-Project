7fcccea4fc540968adbf5cd58fe6a4f261b5d24f
bombeo659 <nqt02112001@gmail.com>
2021-12-07 22:40:29 +0700

    add lab5

---
+++ lab 5/Inc/lab5_timer.h
@@ -0,0 +1,15 @@
+/*
+ * lab5_timer.h
+ *
+ *  Created on: Dec 7, 2021
+ *      Author: QuocTrong
+ */
+
+#ifndef INC_LAB5_TIMER_H_
+#define INC_LAB5_TIMER_H_
+
+void setTimer0(int duration);
+void timer0_run();
+unsigned char get_timer0_flag();
+
+#endif /* INC_LAB5_TIMER_H_ */
